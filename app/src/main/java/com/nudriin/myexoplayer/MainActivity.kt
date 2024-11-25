cae321a05873f61535910a0c93f77a8c9b0fb1b2
Nurdin <mrsunnysummer@gmail.com>
2024-11-26 00:20:29 +0700

    changes: implement background playback service in main activity

--- app/src/main/java/com/nudriin/myexoplayer/MainActivity.kt
+++ app/src/main/java/com/nudriin/myexoplayer/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.nudriin.myexoplayer
 
+import android.content.ComponentName
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -9,6 +10,9 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
 import androidx.media3.common.MediaItem
 import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.session.MediaController
+import androidx.media3.session.SessionToken
+import com.google.common.util.concurrent.MoreExecutors
 import com.nudriin.myexoplayer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -18,18 +22,16 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val videoItem = MediaItem.fromUri("https://github.com/dicodingacademy/assets/releases/download/release-video/VideoDicoding.mp4")
-        val audioItem = MediaItem.fromUri("https://github.com/dicodingacademy/assets/raw/main/android_intermediate_academy/bensound_ukulele.mp3")
-
-        val player = ExoPlayer.Builder(this).build().also { exoPlayer ->
-            exoPlayer.setMediaItem(videoItem)
-            exoPlayer.addMediaItem(audioItem)
-            exoPlayer.prepare()
-        }
-
-        binding.playerView.player = player
-
         hideSystemUI()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val sessionToken = SessionToken(this, ComponentName(this, PlayBackService::class.java))
+        val controllerFeature = MediaController.Builder(this, sessionToken).buildAsync()
+        controllerFeature.addListener({
+            binding.playerView.player = controllerFeature.get()
+        }, MoreExecutors.directExecutor())
     }
 
     private fun hideSystemUI() {
